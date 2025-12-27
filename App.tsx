ccdd00b9ec9d3cc3515f35f31c2032b6f0ab4bae
Jonathan Barbosa <jonathanbazevedo@hotmail.com>
2025-12-27 18:48:19 -0300

    feat: Incluído captação com a câmera

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import MyComponent from './src/navigation';
+import AppRootNavigator from './src/navigation';
+import { AdventuresProvider } from './src/context/adventures';
 
 export default function App() {
 	return (
-		<NavigationContainer theme={DarkTheme}>
-			<StatusBar style='light' />
-			<MyComponent />
-		</NavigationContainer>
+		<AdventuresProvider>
+			<NavigationContainer theme={DarkTheme}>
+				<StatusBar style='light' />
+				<AppRootNavigator />
+			</NavigationContainer>
+		</AdventuresProvider>
 	);
 }
---
+++ src/screens/AdventureForm/index.tsx
@@ -0,0 +1,200 @@
+import { View, Text, Pressable, Alert, Linking, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { colors } from '../../styles/colors';
+import AppHeader from '../../components/AppHeader';
+import { Button, TextInput } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackNavigationProp } from '../../navigation';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { Image } from 'expo-image';
+import { Adventure, useAdventures } from '../../context/adventures';
+
+
+const AdventureForm = () => {
+	const navigation = useNavigation<RootStackNavigationProp>();
+	const [permission, requestPermission] = useCameraPermissions();
+	const cameraRef = useRef<CameraView>(null);
+	const [isCameraActive, setIsCameraActive] = useState(false);
+	const { adventures, addAdventure } = useAdventures();
+	const [form, setForm] = useState<Adventure>({
+		id: '',
+		name: '',
+		description: '',
+		date: '',
+		image: '',
+	});
+
+	const handleInputChange = (key: keyof Adventure, value: string) => {
+		setForm({ ...form, [key]: value });
+	};
+
+	const handleTakePicture = async () => {
+		const photo = await cameraRef.current?.takePictureAsync();
+		setForm({ ...form, image: photo?.uri });
+		setIsCameraActive(false);
+	};
+
+	const handleAddAdventure = () => {
+		addAdventure({ ...form, id: (adventures.length + 1).toString() });
+		navigation.reset({ index: 0, routes: [{ name: 'Adventures' }] });
+	};
+
+	useEffect(() => {
+		if (!!permission && !permission.granted) {
+			requestPermission();
+		}
+	}, [permission]);
+
+	const handleAddImage = () => {
+		if (!!permission && permission.status === 'denied') {
+			return Alert.alert(
+				'Permissão Necessária',
+				'Para utilizar esse recurso, você precisa permitir o acesso à câmera no seu dispositivo',
+				[
+					{ text: 'Cancelar', style: 'cancel' },
+					{ text: 'Abrir Configurações', onPress: () => Linking.openSettings() },
+				]
+			);
+		}
+		return setIsCameraActive(true);
+	};
+
+	return (
+		<>
+			{isCameraActive ? (
+				<>
+					<AppHeader
+						title={form.id ? 'Alterar imagem' : 'Adicionar imagem'}
+						icon='close'
+						onPress={() => setIsCameraActive(false)}
+					/>
+					<CameraView style={{ flex: 1, width: '100%' }} mode='picture' ref={cameraRef} />
+					<View
+						style={{
+							position: 'absolute',
+							bottom: 44,
+							width: '100%',
+							alignItems: 'center',
+							justifyContent: 'space-between',
+							paddingHorizontal: 30,
+						}}
+					>
+						<Pressable
+							onPress={handleTakePicture}
+							style={{
+								backgroundColor: 'transparent',
+								borderWidth: 5,
+								borderColor: colors.primary,
+								width: 80,
+								height: 80,
+								borderRadius: 45,
+								alignItems: 'center',
+								justifyContent: 'center',
+								zIndex: 10,
+							}}
+						>
+							<TextInput.Icon
+								icon='camera'
+								size={30}
+								color={colors.primary}
+								onPress={handleTakePicture}
+							/>
+						</Pressable>
+					</View>
+					
+				</>
+			) : (
+				<>
+					<AppHeader title='Adicionar aventura' showBackButton />
+					<ScrollView style={{ marginHorizontal: 16, marginTop: 16 }}>
+						<TextInput
+							label='Nome'
+							placeholder='Nome da aventura'
+							value={form.name}
+							onChangeText={(text) => handleInputChange('name', text)}
+							style={{ backgroundColor: colors.surface }}
+							mode='outlined'
+							outlineColor={colors.outline}
+							activeOutlineColor={colors.outline}
+							textColor={colors.onSurface}
+							multiline
+						/>
+						<TextInput
+							label='Data'
+							placeholder='XX/XX/XXXX'
+							value={form.date}
+							onChangeText={(text) => handleInputChange('date', text)}
+							style={{ backgroundColor: colors.surface, marginTop: 16 }}
+							mode='outlined'
+							outlineColor={colors.outline}
+							activeOutlineColor={colors.outline}
+							textColor={colors.onSurface}
+							right={<TextInput.Icon icon='calendar' />}
+							keyboardType='email-address'
+						/>
+						<TextInput
+							label='Descrição'
+							placeholder='Descrição da aventura'
+							value={form.description}
+							onChangeText={(text) => handleInputChange('description', text)}
+							style={{ backgroundColor: colors.surface, marginTop: 16 }}
+							mode='outlined'
+							outlineColor={colors.outline}
+							activeOutlineColor={colors.outline}
+							textColor={colors.onSurface}
+							multiline
+							numberOfLines={6}
+						/>
+						{!!form.image && form.image.length > 0 ? (
+							<Image
+								source={{ uri: form.image }}
+								style={{
+									width: '100%',
+									marginTop: 16,
+									aspectRatio: 1,
+									borderWidth: 1,
+									borderColor: colors.outline,
+								}}
+								contentFit='contain'
+							/>
+						) : (
+							<TouchableOpacity onPress={handleAddImage} style={{ height: 80, zIndex: 10 }}>
+								<TextInput
+									label='Adicionar uma imagem'
+									placeholder='Adicionar uma imagem'
+									style={{ backgroundColor: colors.surface, marginTop: 16 }}
+									mode='outlined'
+									outlineColor={colors.outline}
+									activeOutlineColor={colors.outline}
+									textColor={colors.onSurface}
+									right={<TextInput.Icon icon='upload' />}
+									readOnly
+									onPress={handleAddImage}
+								/>
+							</TouchableOpacity>
+						)}
+						<View
+							style={{
+								marginTop: 16,
+								justifyContent: 'flex-end',
+								display: 'flex',
+								flexDirection: 'row',
+							}}
+						>
+							<Button
+								mode='contained'
+								onPress={handleAddAdventure}
+								style={{ backgroundColor: colors.primary }}
+								textColor={colors.black}
+							>
+								Adicionar
+							</Button>
+						</View>
+					</ScrollView>
+				</>
+			)}
+		</>
+	);
+};
+
+export default AdventureForm;
